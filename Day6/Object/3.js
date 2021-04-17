7506ced45070159f9a334ee5100409dfc3974337
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-17 21:31:59 +0545

    Create 3.js

---
+++ Day6/Object/3.js
@@ -0,0 +1,5 @@
+//Create a function that takes an object and returns the keys and values as separate arrays. Return the keys sorted alphabetically, and their corresponding values in the same order.
+function keysAndValues(obj) {
+	return [Object.keys(obj), Object.values(obj)];
+	
+}
