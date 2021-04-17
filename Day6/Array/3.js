4beef774bca1e4083299323eef1b49dd9021b92b
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-04-17 21:21:12 +0545

    Create 3.js

---
+++ Day6/Array/3.js
@@ -0,0 +1,9 @@
+//Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+function arrayOfMultiples (num, length) {
+	let array = new Array();
+	for(let i = 1; i<=length;i++)
+		array.push(num*i);
+	
+	return array;
+	
+}
