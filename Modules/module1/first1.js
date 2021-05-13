3f704596cffc00b30378d2a32e622b8fc3196685
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-05-13 09:50:49 +0545

    Create first1.js

---
+++ Modules/module1/first1.js
@@ -0,0 +1,18 @@
+//exporting using module.exports
+
+function add(a,b)
+{
+    return a+b;
+}
+
+function sub(a,b)
+{
+    return a-b;
+}
+
+module.exports =
+{
+    add: add,
+    subtract: sub,
+}
+
