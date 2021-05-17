a60e7f11b78c8d0a4c19de359ee481bfbc77604c
Sangam796 <61979561+Sangam796@users.noreply.github.com>
2021-05-17 18:28:54 +0545

    Update api.js

--- mongodb/route/api.js
+++ mongodb/route/api.js
@@ -1,26 +1,81 @@
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/MyFirstDatabase";
-const collection1 = "Students";
-const dbName = "MyFirstDatabase";
-let client = undefined;
-let db;
+const express = require("express");
+const router = express.Router();
+const mongoclient = require("../mongo/db");
+let collectionName;
+mongoclient.mongoConnection(() => {
+  collectionName = mongoclient.getCollection();
+});
 
-//this object will have two objects as property, first one to set connectiion to database and anotherone to return collection
-let mongoConnectingObj = {
-  mongoConnection: function (callback) {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, dbclient) => {
-      if (err) throw err;
-      else {
-        client = dbclient;
-        db = client.db(dbName).collection(collection1);
-        console.log("MongoDB connection successful");
-      }
-      return callback();
-    });
-  },
-  getCollection: function () {
-    return db;
-  },
-};
+router.get("/", async (req, res) => {
+  try {
+    const Students = await collectionName;
+    const result = await Students.find({}).toArray();
+    res.send(result);
+  } catch (err) {
+    res.send(err.message);
+    console.log(err.message);
+  }
+});
 
-module.exports = mongoConnectingObj;
+router.get("/:_id", async (req, res) => {
+  try {
+    let id = parseInt(req.params._id);
+    Students = await collectionName;
+    const result = await Students.findOne({ _id: id });
+    res.send(result);
+  } catch (err) {
+    res.send(err.message);
+    console.log(err.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const details = {
+      _id: req.body._id,
+      name: req.body.name,
+      course: req.body.course,
+      yearOfGraduation: req.body.yearOfGraduation,
+    };
+    const result = await collectionName.insertOne(details);
+    res.send(result.ops);
+  } catch (err) {
+    res.send(err.message);
+    console.log(err.message);
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  try {
+    const userId = parseInt(req.params._id);
+    const details = req.body;
+    console.log(details);
+    const result = await collectionName.updateOne(
+      { _id: userId },
+      { $set: details }
+    );
+    console.log("Result modified");
+    res.send(result);
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.send);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  try {
+    const Students = await collectionName;
+    const result = await Students.deleteOne(
+      {
+        _id: parseInt(req.params._id),
+      },
+      1
+    );
+    res.send("Member Deleted");
+  } catch (err) {
+    res.send(err.message);
+    console.log("Error caught");
+  }
+});
+
+module.exports = router;
